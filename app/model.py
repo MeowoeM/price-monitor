2f70d6eba0696b1e96211678d7993befc25c1be8
MeowoeM <liyang1993cn@gmail.com>
2019-10-28 16:07:30 +0900

    v0.1

--- app/model.py
+++ app/model.py
@@ -11,47 +11,71 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
+    tasks = db.relationship("Task", secondary="subscribes")
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.passwordHash, password)
 
+    def subscribe(self, task, time=datetime.utcnow):
+        self.subscribes.append(Subscribe(user=self, task=task, subscribe_time=time))
+
+    def unsubscribe(self, task_id):
+        for task in self.subscribes:
+            if task.id == task_id:
+                self.subscribes.remove(task)
+                return True
+        return False
+
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<User {} [{}]>'.format(self.name, self.id)
 
 # monitoring task
 class Task(db.Model):
     __tablename__ = 'tasks'
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(64))
+    name = db.Column(db.String(64))
     description = db.Column(db.Text)
+    image_url = db.Column(db.String(2048))
 
     url = db.Column(db.String(2048), index=True, unique=True)
     created_time = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     expiring_time = db.Column(db.DateTime, index=True)
+    interval = db.Column(db.Integer, index=True) # frequency, in minutes
+
+    prices = db.relationship("Price", secondary="price_lookup")
+    subscribers = db.relationship("User", secondary="subscribes")
 
     def __repr__(self):
-        return '<Task {}>'.format(self.title)
+        return '<Task {} [{}]>'.format(self.name, self.id)
 
 # prices history
 class Price(db.Model):
     __tablename__ ='prices'
 
     id = db.Column(db.Integer, primary_key=True)
-    price = db.Column(db.Numeric(10, 2))
+    time = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    price = db.Column(db.Numeric(10, 2), index=True)
 
-subscribe_table = db.Table(
-    'subscribes',
-    db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
-    db.Column('task_id', db.Integer, db.ForeignKey('tasks.id')),
-    db.PrimaryKeyConstraint('user_id', 'task_id')
-)
 
-price_lookup_table = db.Table(
-    'price_lookup',
-    db.Column('task_id', db.Integer, db.ForeignKey('tasks.id')),
-    db.Column('price_id', db.Integer, db.ForeignKey('prices.id')),
-    db.PrimaryKeyConstraint('task_id', 'price_id')
-)
+class Subscribe(db.Model):
+    __tablename__ = 'subscribes'
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    task_id = db.Column(db.Integer, db.ForeignKey('tasks.id'), primary_key=True)
+    subscribe_time = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    user = db.relationship("User", backref=db.backref("subscribes", cascade="all, delete-orphan" ))
+    task = db.relationship("Task", backref=db.backref("subscribes", cascade="all, delete-orphan" ))
+
+    def __repr__(self):
+        return '<Subscribe {} <<-->> {} >'.format(self.user.name, self.task.name)
+
+class PriceLookup(db.Model):
+    __tablename__ = 'price_lookup'
+
+    task_id = db.Column(db.Integer, db.ForeignKey('tasks.id'), primary_key=True)
+    price = db.Column(db.Integer, db.ForeignKey('prices.id'), primary_key=True)
